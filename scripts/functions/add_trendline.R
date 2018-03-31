053f410dada47e910dc9ecffaf1df547ef5df068
Gaurav <gaurav.kandlikar@gmail.com>
2018-03-30 19:57:14 -0700

    updating fig1 for editor comments

--- scripts/functions/add_trendline.R
+++ scripts/functions/add_trendline.R
@@ -16,7 +16,7 @@ add_line <- function(trait, df, log = F, digits = 3, pos=NULL) {
   if (pval < 0.05) {abline(net_lm, lwd = 1.5,lty=2)}
 
     to_write <- bquote(atop(italic(R)^2 == .(rsq), 
-                        italic(p) * .(format(pval2))))
+                        italic(P) * .(format(pval2))))
     text(x = 1750, y = yy, pos = 1, offset = 1, 
          labels = to_write, bty = 'n', cex = 1.7)
   
