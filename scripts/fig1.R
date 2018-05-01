e62906d30e62a7b428cde8d071469ec599794e6b
Marcel Vaz <mcvaz@ucla.edu>
2018-05-01 12:07:11 -0700

    graphic comstics update

--- scripts/fig1.R
+++ scripts/fig1.R
@@ -1,75 +1,86 @@
 source("scripts/functions/add_trendline.R")
 
-# tiff("fig1.tiff",width=11,height=7,res=600,units="in",compression="lzw",bg="white",type="cairo")
+tiff("fig1.tiff",width=11,height=7,res=600,units="in",compression="lzw",bg="white",type="cairo")
 
 
-par(mfrow = c(2,4), oma = c(8,1,5,1), mar = c(0, 5, 2, 1), 
+par(mfrow = c(2,4), oma = c(8,1,1,1), mar = c(0, 5, 2, 1), 
     cex.lab = 1.5, cex.main = 2, xpd = F, cex.axis = 1.25)
+
+lcex = 1.3
+sp_by_location$pchvec = pchvec
+sp_by_location$colvec = NA
+cols = grey(alpha=0.7,c(1,.5,0))
+for(i in 1:3){sp_by_location[sp_by_location$pchvec==(21:23)[i],"colvec"] = cols[i]}
+head(sp_by_location)
 
 # SLA
 plot(sp_by_location$log_sla~jitter(sp_by_location$elevation, factor = .3),
-     xlab = "", pch = pchvec, bty = "n", xaxt = "n", bg = colvec, cex = 1.5,
-     ylab = expression(paste('log'[10],'(SLA) (','cm'^2,'g'^-1,')')), las = 1,
-     ylim=c(1.9,2.9))
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", xaxt = "n", 
+     bg = sp_by_location$colvec, cex = 1.5, ylim = c(1.9,2.9),
+     ylab = expression(paste('log'[10],'(SLA) (','cm'^2,'g'^-1,')')), las = 1)
 add_line(trait = "log_sla", df = sp_by_location, log = F)
-box(lwd = 0.5)
-mtext(side = 3, text = "A", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "a", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # LDMC
 plot(sp_by_location$log_ldmc~jitter(sp_by_location$elevation, factor = .3), 
-     xlab = "", pch = pchvec, bty = "n", xaxt = "n", bg = colvec, cex = 1.5,
-     ylab = expression(paste('log'[10],'(LDMC) (','mg g'^-1,')')), las = 1,
-     ylim=c(1.05,1.75))
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", xaxt = "n", 
+     bg = sp_by_location$colvec, cex = 1.5, ylim = c(1.05,1.75),
+     ylab = expression(paste('log'[10],'(LDMC) (','mg g'^-1,')')), las = 1)
 add_line(trait = "log_ldmc", df = sp_by_location, log = F)
-box(lwd = 0.5)
-mtext(side = 3, text = "B", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "b", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Leaf Area
 plot(sp_by_location$log_leaf_area~jitter(sp_by_location$elevation, factor = .3), 
-     xlab = "", pch = pchvec, bty = "n", xaxt = "n", bg = colvec, cex = 1.5,
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", xaxt = "n", 
+     bg = sp_by_location$colvec, cex = 1.5,
      ylab = expression(paste('log'[10],'(Leaf Area) (','cm'^2,')')), las = 1,
-     ylim=c(.2,3.1))
+     ylim=c(.2,3.2))
 add_line(trait = "log_leaf_area", df = sp_by_location, log = F)
-box(lwd = 0.5)
-mtext(side = 3, text = "C", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "c", adj = 0.02, line = -1, font = 2, cex = lcex)
 
-# Specific Force
+# Leaf toughness
 plot(sp_by_location$log_specific_force~jitter(sp_by_location$elevation, factor = .3), cex = 1.5,
-     xlab = "", pch = pchvec, bty = "n", bg = colvec,  las = 1,
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", 
+     bg = sp_by_location$colvec,  las = 1, ylim = c(-0.2,0.9),
      ylab = expression(paste('log'[10],'(Leaf Toughness) (','N m'^-2,')')))
 add_line(trait = "log_specific_force", df = sp_by_location, log = F, pos=0.1)
-box(lwd = 0.5)
-mtext(side = 3, text = "D", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "d", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Leaf N
 plot(sp_by_location$lnc~jitter(sp_by_location$elevation, factor = .3), 
-     xlab = "", pch = pchvec, bty = "n", bg = colvec, cex = 1.5, las = 1,
-     ylab = "Leaf Nitrogen (%)")
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", 
+     bg = sp_by_location$colvec, cex = 1.5, las = 1, ylab = "Leaf Nitrogen (%)")
 add_line(trait = "lnc", df = sp_by_location, log = F)
-box(lwd = 0.5)
-mtext(side = 3, text = "E", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "e", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Stem Density
-plot(sp_by_location$stem_density~jitter(sp_by_location$elevation, factor = .3), cex = 1.5,
-     xlab = "", pch = pchvec, bty = "n", bg = colvec,  las = 1,
+plot(sp_by_location$stem_density~jitter(sp_by_location$elevation, factor = .3),
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", 
+     bg = sp_by_location$colvec,  las = 1, cex = 1.5,
      ylab = expression(paste('Stem density (','g cm'^-3,')')))
 add_line(trait = "stem_density", df = sp_by_location, log = F, pos=.65)
-box(lwd = 0.5)
-mtext(side = 3, text = "F", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "f", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Seed size
 plot(sp_by_location$log_seed_area~jitter(sp_by_location$elevation, factor = .3), 
-     xlab = "", pch = pchvec, bty = "n", bg = colvec, cex = 1.5, las = 1,
-     ylab = expression(paste('log'[10],'(Seed area) (',mu,'m'^2,')')))
+     xlab = "", pch = sp_by_location$pchvec, bty = "l", bg = sp_by_location$colvec, cex = 1.5, las = 1,
+     ylab = expression(paste('log'[10],'(Seed area) (',mu,'m'^2,')')), 
+     ylim = c(4.5,8))
 add_line(trait = "log_seed_area", df = sp_by_location, log = F)
-box(lwd = 0.5)
-mtext(side = 3, text = "G", adj = 0.02, line = 0, font = 2)
+#box(lwd = 0.5)
+mtext(side = 3, text = "g", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Legend
 plot(sp_by_location$log_seed_area~jitter(sp_by_location$elevation, factor = .3), 
-     xlab = "", bty = "n", bg = colvec, type="n", ylab = "", xaxt="n", yaxt="n")
-legend(x=0, y=6.5, pch = pchs, 
-       pt.bg = c("black", "grey", "white"), xpd = NA, y.intersp = 1,
+     xlab = "", bty = "n", type="n", ylab = "", xaxt="n", yaxt="n")
+legend(x=0, y=6.5, pch = c(22,23,21), 
+       pt.bg = cols[c(2,3,1)], xpd = NA, y.intersp = 1,
        legend = levels(sp_by_location$growth_form), horiz = F, 
        bty = "n", pt.cex = 1.5, cex = 1.7, x.intersp = 0.8)
 
@@ -78,4 +89,4 @@ legend(x=0, y=6.5, pch = pchs,
 mtext(side = 1, text = "Altitude (m asl)", outer = T, line = 5, cex = 1.25)
 
 
-# dev.off()
+dev.off()
--- scripts/functions/add_trendline.R
+++ scripts/functions/add_trendline.R
@@ -14,9 +14,10 @@ add_line <- function(trait, df, log = F, digits = 3, pos=NULL) {
   if(pval<0.01) {pval2 = "< 0.01"} else {pval2 = paste("=",pval)}
   if(is.null(pos)) {yy = par("usr")[4]} else {yy = pos}
   if (pval < 0.05) {abline(net_lm, lwd = 1.5,lty=2)}
-
-    to_write <- bquote(atop(italic(R)^2 == .(rsq), 
-                        italic(P) * .(format(pval2))))
+  if(rsq<0.01){to_write <- bquote(atop(italic(R)^2 < 0.01, 
+                italic(P) * .(format(pval2))))} else {
+                  to_write <- bquote(atop(italic(R)^2 == .(rsq), 
+                    italic(P) * .(format(pval2))))}
     text(x = 1750, y = yy, pos = 1, offset = 1, 
          labels = to_write, bty = 'n', cex = 1.7)
   
