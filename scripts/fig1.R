e983eb3d80ce7a00b9a2708460bb62e96a28a7d5
Marcel Vaz <mcvaz@ucla.edu>
2018-05-22 08:18:50 -0700

    fig.1 update

--- scripts/fig1.R
+++ scripts/fig1.R
@@ -14,7 +14,7 @@ for(i in 1:3){sp_by_location[sp_by_location$pchvec==(21:23)[i],"colvec"] = cols[
 head(sp_by_location)
 
 # SLA
-plot(sp_by_location$log_sla~jitter(sp_by_location$elevation, factor = .3),
+plot(sp_by_location$log_sla~jitter(sp_by_location$elevation, factor = .5),
      xlab = "", pch = sp_by_location$pchvec, bty = "l", xaxt = "n", 
      bg = sp_by_location$colvec, cex = 1.5, ylim = c(1.9,2.9),
      ylab = expression(paste('log'[10],'(SLA) (','cm'^2,'g'^-1,')')), las = 1)
@@ -23,7 +23,7 @@ add_line(trait = "log_sla", df = sp_by_location, log = F)
 mtext(side = 3, text = "a", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # LDMC
-plot(sp_by_location$log_ldmc~jitter(sp_by_location$elevation, factor = .3), 
+plot(sp_by_location$log_ldmc~jitter(sp_by_location$elevation, factor = .5), 
      xlab = "", pch = sp_by_location$pchvec, bty = "l", xaxt = "n", 
      bg = sp_by_location$colvec, cex = 1.5, ylim = c(1.05,1.75),
      ylab = expression(paste('log'[10],'(LDMC) (','mg g'^-1,')')), las = 1)
@@ -32,7 +32,7 @@ add_line(trait = "log_ldmc", df = sp_by_location, log = F)
 mtext(side = 3, text = "b", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Leaf Area
-plot(sp_by_location$log_leaf_area~jitter(sp_by_location$elevation, factor = .3), 
+plot(sp_by_location$log_leaf_area~jitter(sp_by_location$elevation, factor = .5), 
      xlab = "", pch = sp_by_location$pchvec, bty = "l", xaxt = "n", 
      bg = sp_by_location$colvec, cex = 1.5,
      ylab = expression(paste('log'[10],'(Leaf area) (','cm'^2,')')), las = 1,
@@ -42,7 +42,7 @@ add_line(trait = "log_leaf_area", df = sp_by_location, log = F)
 mtext(side = 3, text = "c", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Leaf force to punch
-plot(sp_by_location$log_specific_force~jitter(sp_by_location$elevation, factor = .3), cex = 1.5,
+plot(sp_by_location$log_specific_force~jitter(sp_by_location$elevation, factor = .5), cex = 1.5,
      xlab = "", pch = sp_by_location$pchvec, bty = "l", 
      bg = sp_by_location$colvec,  las = 1, ylim = c(-0.2,0.9),
      ylab = expression(paste('log'[10],'(Leaf force to punch) (','N m'^-2,')')))
@@ -51,7 +51,7 @@ add_line(trait = "log_specific_force", df = sp_by_location, log = F, pos=0.1)
 mtext(side = 3, text = "d", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Leaf N
-plot(sp_by_location$lnc~jitter(sp_by_location$elevation, factor = .3), 
+plot(sp_by_location$lnc~jitter(sp_by_location$elevation, factor = .5), 
      xlab = "", pch = sp_by_location$pchvec, bty = "l", 
      bg = sp_by_location$colvec, cex = 1.5, las = 1, ylab = "Leaf nitrogen concentration (%)")
 add_line(trait = "lnc", df = sp_by_location, log = F)
@@ -59,7 +59,7 @@ add_line(trait = "lnc", df = sp_by_location, log = F)
 mtext(side = 3, text = "e", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Stem Density
-plot(sp_by_location$stem_density~jitter(sp_by_location$elevation, factor = .3),
+plot(sp_by_location$stem_density~jitter(sp_by_location$elevation, factor = .5),
      xlab = "", pch = sp_by_location$pchvec, bty = "l", 
      bg = sp_by_location$colvec,  las = 1, cex = 1.5,
      ylab = expression(paste('Stem-specific density (','g cm'^-3,')')))
@@ -68,7 +68,7 @@ add_line(trait = "stem_density", df = sp_by_location, log = F, pos=.65)
 mtext(side = 3, text = "f", adj = 0.02, line = -1, font = 2, cex = lcex)
 
 # Seed size
-plot(sp_by_location$log_seed_area~jitter(sp_by_location$elevation, factor = .3), 
+plot(sp_by_location$log_seed_area~jitter(sp_by_location$elevation, factor = .5), 
      xlab = "", pch = sp_by_location$pchvec, bty = "l", bg = sp_by_location$colvec, cex = 1.5, las = 1,
      ylab = expression(paste('log'[10],'(Seed area) (',mu,'m'^2,')')), 
      ylim = c(4.5,8))
